a0e162c59c762115c87af62f8662c85f7f47b24e
brandonalfaro1324 <alfarobrandon25@gmail.com>
2024-02-22 15:42:26 -0600

    Adding files for project

---
+++ project1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unistd.h>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+
+
+// Include files created for this project
+#include "cpu.h"
+#include "cpu.cpp"
+#include "memory.h"
+#include "memory.cpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) { 
+    cout << "Hello World" << endl;
+    return 0;
+}
